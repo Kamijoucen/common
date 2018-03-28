f473ccade44dd84129857f9e06387ea703c84eba
Kamijoucen <lisicen12138@outlook.com>
2018-03-28 17:13:41 +0800

    md5

--- src/com/kamijoucen/utils/Utils.java
+++ src/com/kamijoucen/utils/Utils.java
@@ -179,27 +179,34 @@ public final class Utils {
     }
 
 
+    /**
+     * 生成字符串的MD5
+     * @param s
+     * @return
+     */
     public static String MD5(String s) {
+        if (isBlankVal(s)) {
+            return null;
+        }
         char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         byte[] btInput = s.getBytes();
-        MessageDigest mdInst = null;
         try {
-            mdInst = MessageDigest.getInstance("MD5");
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-        mdInst.update(btInput);
-        byte[] md = mdInst.digest();
-        int j = md.length;
-        char str[] = new char[j * 2];
-        int k = 0;
-        for (int i = 0; i < j; i++) {
-            byte byte0 = md[i];
-            str[k++] = hexDigits[byte0 >>> 4 & 0xf];
-            str[k++] = hexDigits[byte0 & 0xf];
-        }
-        return new String(str);
     }
 
 }
