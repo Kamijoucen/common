e69073987e080bd235aefd48d975a6150834aa51
Kamijoucen <lisicen12138@outlook.com>
2018-03-28 17:08:01 +0800

    md5

--- src/com/kamijoucen/utils/Utils.java
+++ src/com/kamijoucen/utils/Utils.java
@@ -1,5 +1,6 @@
 package com.kamijoucen.utils;
 
+import java.security.MessageDigest;
 import java.util.*;
 
 public final class Utils {
@@ -175,6 +176,30 @@ public final class Utils {
             success = obj.equals(objs[i]);
         }
         return success;
+    }
+
+
+    public static String MD5(String s) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        byte[] btInput = s.getBytes();
+        MessageDigest mdInst = null;
+        try {
+            mdInst = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        mdInst.update(btInput);
+        byte[] md = mdInst.digest();
+        int j = md.length;
+        char str[] = new char[j * 2];
+        int k = 0;
+        for (int i = 0; i < j; i++) {
+            byte byte0 = md[i];
+            str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+            str[k++] = hexDigits[byte0 & 0xf];
+        }
+        return new String(str);
     }
 
 }
